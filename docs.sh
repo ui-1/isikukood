c4b1d8823ecc8c69123fb310df879570196b3141
ui-1 <ui-1@posteo.org>
2023-06-08 00:01:53 +0300

    Generate entire readme using docs.sh

--- docs.sh
+++ docs.sh
@@ -1,2 +1,18 @@
-pydoc-markdown -I isikukood/ -m functions -m isikukood -m assertions --render-toc > temp.md
-sed -i 's/<a/---\n<a/' temp.md
+#!/bin/bash
+
+cat > README.md << 'EOF'
+# Isikukood
+
+Small Estonian social security number library (I know they're not really SSNs but I don't have a better English name for them)
+
+# Installation
+
+Simply run the following:
+```bash
+pip install isikukood
+```
+
+EOF
+
+pydoc-markdown -I isikukood/ -m functions -m isikukood -m assertions --render-toc >> README.md
+sed -i 's/<a/---\n<a/' README.md
