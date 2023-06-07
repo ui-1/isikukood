f859a0c0c974175334c97a3659345ade3e63c4b3
ui-1 <ui-1@posteo.org>
2023-06-07 16:42:28 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="isikukood",
+    version="1.0.0",
+    description="Estonian SSN library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ui-1/isikukood",
+    author="ui-1",
+    author_email="ui-1@posteo.org",
+    license="GPLv3",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries"
+    ],
+    packages=["isikukood"],
+    include_package_data=True,
+    install_requires=["multipledispatch"]
+)
